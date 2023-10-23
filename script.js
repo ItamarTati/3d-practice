c5e98340447b42accfa767d2a8cab73b4a1198d5
ItamarTati <itamar.tati1994@gmail.com>
2023-10-23 10:22:42 +0100

    added 3d objects

---
+++ script.js
@@ -0,0 +1,35 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+let object;
+let controls;
+
+const loader = new GLTFLoader();
+loader.load(
+    '/public/macbookpro/scene.gltf',
+    function (gltf) {
+        object = gltf.scene;
+        scene.add(object);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        console.error(error);
+    }
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth - 20, window.innerHeight);
+
+document.getElementById("container3D").appendChild(renderer.domElement);
+document.getElementById("container3D").style.cursor = 'grabbing'
+document.getElementById("container3D").style.width = '100%'
